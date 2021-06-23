747e38f3e25b584b423b35e74832dd39940ff5e6
dagmar <dagmarlewis8@gmail.com>
2021-06-23 13:49:28 -0500

    update links

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,53 +1,56 @@
-import website from '../assets/website.png'
-import instagram from '../assets/instagram.png'
-import appstore from '../assets/IMG_20201124_112759.png'
-import youtube from '../assets/images (3).jpeg'
-import dribbble from '../assets/dribbble.png'
-import github from '../assets/github.png'
-import telegram from '../assets/IMG_20201124_104554.png'
+import website from "../assets/website.png";
+import instagram from "../assets/instagram.png";
+import appstore from "../assets/IMG_20201124_112759.png";
+import youtube from "../assets/images (3).jpeg";
+import dribbble from "../assets/dribbble.png";
+import github from "../assets/github.png";
+import telegram from "../assets/IMG_20201124_104554.png";
+import twitter from "../assets/twitter.png ";
 
-const items = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "#"
-},
-{
-    "title": "GitHub",
-    "subtitle": "@devgossips | 🏠 of my open-source projects",
-    "image": github,
-    "link": "https://github.com/devgossips"
-},
-{
-    "title": "Instagram",
-    "subtitle": "@virtuonic100 | Hub of my awesome 😎pics",
-    "image": instagram,
-    "link": "https://instagram.com/virtuonic100"
-},
-{
-    "title": "Apps",
-    "subtitle": "Hub of my awesome🔥 Apps",
-    "image": appstore,
-    "link": "#"
-},
-{
-    "title": "YouTube",
-    "subtitle": "@Sagnik Sahoo | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"
-},
+const items = [
+  {
+    title: "Portfolio",
+    subtitle: "Look at my work!",
+    image: website,
+    link: "https://dagmar.dev/",
+  },
+  {
+    title: "GitHub",
+    subtitle: "@dagmar-dev| 🏠 Personal Projects",
+    image: github,
+    link: "https://github.com/dagmar-dev",
+  },
+  {
+    title: "Instagram",
+    subtitle: "@darkcreati0n | Hub of my awesome 😎pics",
+    image: instagram,
+    link: "https://instagram.com/darkcreati0n",
+  },
+  {
+    title: "twitter",
+    subtitle: "@dagmar_dev | Hub of my thoughts",
+    image: twitter,
+    link: "https://twitter.com/dagmar_dev",
+  },
+  {
+    title: "YouTube",
+    subtitle: "@Sagnik Sahoo | Official channel of mine",
+    image: youtube,
+    link: "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg",
+  },
 
-{
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic"
-},
-{
-    "title": "Telegram",
-    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
-    "image": telegram,
-    "link": "https://telegram.me/virtuonic"
-}]
+  {
+    title: "Dribbble",
+    subtitle: "@virtuonic | Hub to the Shots of my 😎 apps ",
+    image: dribbble,
+    link: "https://dribbble.com/virtuonic",
+  },
+  {
+    title: "Telegram",
+    subtitle: "@virtuonic | Chat with me instantly, Don't hesitate! ",
+    image: telegram,
+    link: "https://telegram.me/virtuonic",
+  },
+];
 
-export default items
+export default items;
