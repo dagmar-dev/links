1f1e48617ba15fb02610ae140109ac85c864e2f1
dark <75873640+darkcreati0n@users.noreply.github.com>
2022-01-16 19:42:30 -0500

    instagram change

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,40 +1,40 @@
-import website from "../assets/website.png";
-import instagram from "../assets/instagram.png";
-import github from "../assets/github.png";
-import twitter from "../assets/twitter.png";
-import phone from "../assets/phone.png";
+import website from '../assets/website.png';
+import instagram from '../assets/instagram.png';
+import github from '../assets/github.png';
+import twitter from '../assets/twitter.png';
+import phone from '../assets/phone.png';
 
 const items = [
-  {
-    title: "Contact",
-    subtitle: "Save me in your contacts",
-    image: phone,
-    link: "https://www.dagmar.dev/store/Lewis_Dagmar.vcf",
-  },
-  {
-    title: "Portfolio",
-    subtitle: "Look at my work!",
-    image: website,
-    link: "https://dagmar.dev/",
-  },
-  {
-    title: "GitHub",
-    subtitle: "@dagmar-dev| 🏠 Personal Projects",
-    image: github,
-    link: "https://github.com/dagmar-dev",
-  },
-  {
-    title: "Instagram",
-    subtitle: "@darkcreati0n | Hub of my awesome 😎pics",
-    image: instagram,
-    link: "https://instagram.com/darkcreati0n",
-  },
-  {
-    title: "twitter",
-    subtitle: "@dagmar_dev | Hub of my thoughts",
-    image: twitter,
-    link: "https://twitter.com/dagmar_dev",
-  },
+    {
+        title: 'Contact',
+        subtitle: 'Save me in your contacts',
+        image: phone,
+        link: 'https://www.dagmar.dev/store/Lewis_Dagmar.vcf',
+    },
+    {
+        title: 'Portfolio',
+        subtitle: 'Look at my work!',
+        image: website,
+        link: 'https://dagmar.dev/',
+    },
+    {
+        title: 'GitHub',
+        subtitle: '@dagmar-dev| 🏠 Personal Projects',
+        image: github,
+        link: 'https://github.com/dagmar-dev',
+    },
+    {
+        title: 'Instagram',
+        subtitle: ' | Hub of my awesome 😎pics',
+        image: instagram,
+        link: '',
+    },
+    {
+        title: 'twitter',
+        subtitle: '@dagmar_dev | Hub of my thoughts',
+        image: twitter,
+        link: 'https://twitter.com/dagmar_dev',
+    },
 ];
 
 export default items;
